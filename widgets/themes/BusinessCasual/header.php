bad6bb3f74ae565b26261c8a416f0f3054ae850c
Amy Maker <amymaker@tests-MacBook-Air-98.local>
2017-11-29 14:36:59 -0800

    images for instruments
    uploaded all the images for instruments

--- widgets/themes/BusinessCasual/header.php
+++ widgets/themes/BusinessCasual/header.php
@@ -53,6 +53,9 @@
             <li class="nav-item px-lg-4">
               <a class="nav-link text-uppercase text-expanded" href="daily.php">Daily</a>
             </li>  
+                          <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="instrument_list.php">Instruments</a>
+            </li> 
           </ul>
         </div>
       </div>
